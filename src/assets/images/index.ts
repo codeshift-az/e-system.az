e4ffb08c12de9d1d86038c810de12fee449c4142
nihad memmedzade <nihad.mmmdzad@bk.ru>
2024-09-29 03:17:08 +0400

    Images added

---
+++ src/assets/images/index.ts
@@ -0,0 +1,68 @@
+
+// Logo
+export { default as Logo } from "./logo/logo-light.png";
+export { default as Logo2 } from "./logo/logo.png";
+export { default as Logo3 } from "./logo/1.png";
+export { default as Logo4 } from "./logo/2.png";
+export { default as Logo5 } from "./logo/3.png";
+export { default as Logo6 } from "./logo/4.png";
+export { default as Logo7 } from "./logo/5.png";
+
+
+//? Home Sections 
+
+// Banner
+export { default as BannerBackground } from "./home/banner/25.png";
+export { default as BannerBackground2 } from "./home/banner/26.png";
+export { default as BannerBackground3 } from "./home/banner/18.png";
+
+
+// AboutArea
+export { default as AboutShape } from "./home/aboutArea/9.png";
+export { default as AboutAreaImg } from "./home/aboutArea/16.png";
+export { default as AboutAreaImg2 } from "./home/aboutArea/11.png";
+
+// Subscribe Area
+export { default as SubscribeShape } from "./home/subscribeArea/14.png";
+
+// Features
+export { default as FeaturesBackground } from "./home/features/3.jpg";
+export { default as FeaturesShape } from "./home/features/17.png";
+
+// Choose us Area
+export { default as ChooseUsAreaImg } from "./home/chooseUsArea/2.png";
+
+// Team Area
+export { default as TeamAreaImg } from "./home/teamArea/1.jpg";
+export { default as TeamAreaImg2 } from "./home/teamArea/2.jpg";
+export { default as TeamAreaImg3 } from "./home/teamArea/3.jpg";
+export { default as TeamAreaBackground } from "./home/teamArea/5.png";
+
+// Pricing
+export { default as PricingBackground } from "./home/pricing/16.png";
+
+// Blog
+export { default as BlogImg } from "./home/blog/1.jpg";
+export { default as BlogImg2 } from "./home/blog/2.jpg";
+export { default as BlogImg3 } from "./home/blog/3.jpg";
+
+// Testimonials
+export { default as TestimonialsImg } from "./home/testimonials/2.jpg";
+export { default as TestimonialsImg2 } from "./home/testimonials/3.jpg";
+export { default as TestimonialsImg3 } from "./home/testimonials/7.jpg";
+
+//? Home Sections end
+
+
+//? Contact Page
+
+export { default as ContactBackground } from "./contact/9.jpg";
+export { default as ContactImg } from "./contact/contact.png";
+
+//? Contact Page end
+
+//? Not Found Page
+
+export { default as NotFoundBackground } from "./notFound/4.jpg";
+
+//? Not Found Page end
